bde721f31f331b7c5a180e371ae12f43dde06d66
dillbyrne <dillbyrne@users.noreply.github.com>
2016-02-18 14:54:37 +0000

    version 0.1

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+
+BIN=randdns.sh
+DIR=randdns
+RUNLINK=/etc/network/if-up.d/randdns
+
+function checkUser
+{
+	if [[ $EUID -ne 0 ]]; 
+	then
+		
+		echo "Install Aborted\nThis script must be run as root" 1>&2
+		exit 1
+	fi
+}
+
+function install
+{
+	echo "Changing ownership of files to root"
+	chown -R root:root $DIR
+	chown root:root $BIN
+
+	
+	echo "Copying  files to the correct directories"
+	cp -R $DIR /etc/
+	cp $BIN /usr/sbin/randdns
+
+	echo "Changing permissions"
+	chmod 700 /usr/sbin/randdns
+	chmod -R 600 /etc/randDNS/*
+
+	echo "Creating Symbolic link"
+	ln -s /usr/sbin/randdns $RUNLINK
+
+	echo "Install finished, DNS servers will be changed when network is brought up"
+
+}
+
+
+function main
+{
+
+	checkUser
+	install
+
+}
+
+main
---
+++ randdns.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Randomly selects 2 DNS Servers from a user defined list
+# and applies them each time the network is brought up
+#
+# Licence GNU GPL v3+
+# dillbyrne 2016
+
+
+#CONF=/etc/randDNS/randdns.conf
+CONF=randDNS/randdns.conf
+
+function getRandomDNSServer
+{
+	# ignore comments and blank lines in server list
+	echo $(grep  -v -e '^#' -e '^$' $1 | shuf | tail -n 1)
+}
+
+function setRandomDNSServers
+{
+	getRandomDNSServer $SERVERS > $OUTFILE
+	# avoid selecting the same server twice
+	grep -vf $OUTFILE $SERVERS | getRandomDNSServer >> $OUTFILE
+}
+
+
+function Log
+{
+	if [[ $1 -eq 0 ]];
+	then
+		echo $(date) $(cat $OUTFILE) >> $LOGFILE
+	fi
+}
+
+
+function main
+{
+	source $CONF
+	setRandomDNSServers
+	Log $LOGLEVEL
+}
+
+main
